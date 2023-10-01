5498a226ee76b34b5609766563bee3b8476567fe
Konrad Karczewski <konjkar@gmail.com>
2023-10-01 12:58:31 -0400

    world map zooming added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { useEffect } from 'react';
+
 import './App.css'
 
 import Navbar from './components/Navbar'
@@ -8,8 +10,84 @@ import { guitarData } from './data'
 import { maskData } from './data'
 import { pickupData } from './data'
 
+import worldMap from './assets/Worldmap.jpg'
+
 function App() {
   const [count, setCount] = useState(0)
+
+  function imageZoom(imgID, resultID) {
+    var img, lens, result, cx, cy;
+    img = document.getElementById(imgID);
+    result = document.getElementById(resultID);
+    /*create lens:*/
+    lens = document.createElement("DIV");
+    lens.setAttribute("class", "img-zoom-lens");
+    /*insert lens:*/
+    img.parentElement.insertBefore(lens, img);
+    /*calculate the ratio between result DIV and lens:*/
+    cx = result.offsetWidth / lens.offsetWidth;
+    cy = result.offsetHeight / lens.offsetHeight;
+    /*set background properties for the result DIV:*/
+    result.style.backgroundImage = "url('" + img.src + "')";
+    result.style.backgroundSize = (img.width * cx) + "px " + (img.height * cy) + "px";
+    /*execute a function when someone moves the cursor over the image, or the lens:*/
+    lens.addEventListener("mousemove", moveLens);
+    img.addEventListener("mousemove", moveLens);
+    /*and also for touch screens:*/
+    lens.addEventListener("touchmove", moveLens);
+    img.addEventListener("touchmove", moveLens);
+    function moveLens(e) {
+      var pos, x, y;
+      /*prevent any other actions that may occur when moving over the image:*/
+      e.preventDefault();
+      /*get the cursor's x and y positions:*/
+      pos = getCursorPos(e);
+      /*calculate the position of the lens:*/
+      x = pos.x - (lens.offsetWidth / 2);
+      y = pos.y - (lens.offsetHeight / 2);
+      /*prevent the lens from being positioned outside the image:*/
+      if (x > img.width - lens.offsetWidth) {x = img.width - lens.offsetWidth;}
+      if (x < 0) {x = 0;}
+      if (y > img.height - lens.offsetHeight) {y = img.height - lens.offsetHeight;}
+      if (y < 0) {y = 0;}
+      /*set the position of the lens:*/
+      lens.style.left = x + "px";
+      lens.style.top = y + "px";
+      /*display what the lens "sees":*/
+      result.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
+      
+      /*make the result follow the cursor*/
+      result.style.top = (pos.y + 32) + "px";
+      result.style.left = (pos.x + 32) + "px";
+
+    }
+    function getCursorPos(e) {
+      var a, x = 0, y = 0;
+      e = e || window.event;
+      /*get the x and y positions of the image:*/
+      a = img.getBoundingClientRect();
+      /*calculate the cursor's x and y coordinates, relative to the image:*/
+      x = e.pageX - a.left;
+      y = e.pageY - a.top;
+      /*consider any page scrolling:*/
+      x = x - window.pageXOffset;
+      y = y - window.pageYOffset;
+      return {x : x, y : y};
+    }
+  }
+
+    useEffect(() => {
+    imageZoom('myimage', 'myresult');
+    updatePosition;
+  }, []);  // The empty array means this useEffect runs once after initial render
+
+
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const updatePosition = (e) => {
+    setPosition({ x: e.clientX, y: e.clientY });
+  };
+
 
   return (
     <>
@@ -38,15 +116,15 @@ function App() {
           
           </div>
           <div className = "enemy-stat">
-            <div className="label"> <h4> HP </h4> </div>
+            <div className="label"> <h4> HP: </h4> </div>
             <div className="name"> <h4>{hp}</h4> </div>
           </div>
           <div className = "enemy-stat">
-            <div className="label"> <h4> PTS </h4> </div>
+            <div className="label"> <h4> PTS: </h4> </div>
             <div className="name"> <h4> {pts} </h4> </div>
           </div>
           <div className = "enemy-stat">
-            <div className="label"> <h4>DNGR </h4>  </div>
+            <div className="label"> <h4>DNGR: </h4>  </div>
             <div className="name"> <h4> {dngr} </h4> </div>
           </div>
           <div className = "enemy-desrciption">
@@ -162,8 +240,22 @@ function App() {
 
        </div>
 
+       <div className="section-header" id="pickups"><h1>WORLD MAP</h1></div>
+
+       <div className="map-container">
+
+      <div className="img-zoom-container">
+        <div><img id="myimage" className="map_image" src={worldMap} width="100%" /></div>
+        <div  id="myresult" className="img-zoom-result"></div>
+      </div> 
+
+       </div>
+
     </>
+
   )
+
+
 }
 
 export default App
